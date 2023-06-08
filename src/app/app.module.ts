73823863723d845d190fb31d5c12dad9d474f7c9
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-07 20:00:21 -0600

    Update declarations and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { SinglePostComponent } from './components/single-post/single-post.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FourNotFourComponent } from './components/four-not-four/four-not-four.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
     HomeComponent,
-    SinglePostComponent
+    SinglePostComponent,
+    FourNotFourComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent }, // Load HomeComponent (localhost:4200)
-      { path: 'post/:id/:title', component: SinglePostComponent}, // Load SinglePostComponent (localhost:4200/post/<id>/<title>)
-      { path: 'posts', component: PostListComponent} // Load PostListComponent (localhost:4200/posts)
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
