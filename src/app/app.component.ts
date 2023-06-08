1a96c59082de71d82ffa96d5226174d53aefd147
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-07 19:03:45 -0600

    Add Observable const to return several values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reddy-blog-client';
+
+  ngOnInit(): void {
+
+    // Creates an observable
+    const obsTest$ = new Observable(observer => {
+
+      // Return several values from the Observable by using the next() method
+      observer.next('Returned 1 from the Observable');
+      observer.next('Returned 2 from the Observable');
+      observer.next('Returned 3 from the Observable');
+    }).subscribe(value => {
+      console.log(value);
+    });
+  }
+
 }
