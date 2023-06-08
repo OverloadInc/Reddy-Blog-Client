192ab8f67a5080dcc0a73128f76d3aea3b748dde
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-07 19:04:34 -0600

    Add RouterModule paths to load components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { SinglePostComponent } from './components/single-post/single-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    HomeComponent,
+    SinglePostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent }, // Load HomeComponent (localhost:4200)
+      { path: 'post/:id/:title', component: SinglePostComponent}, // Load SinglePostComponent (localhost:4200/post/<id>/<title>)
+      { path: 'posts', component: PostListComponent} // Load PostListComponent (localhost:4200/posts)
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
