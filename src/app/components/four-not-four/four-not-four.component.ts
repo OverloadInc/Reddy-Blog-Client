9f6e416283069dfdc31a1c1ab6b9d0cb665e31c5
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-07 19:59:19 -0600

    Add 404 Component to manage not found pages

---
+++ src/app/components/four-not-four/four-not-four.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-four-not-four',
+  templateUrl: './four-not-four.component.html',
+  styleUrls: ['./four-not-four.component.css']
+})
+export class FourNotFourComponent {
+
+}
