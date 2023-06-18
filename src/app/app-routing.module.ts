afe2296345be64982fed27e05c91f9e0951c826f
Juan Alberto Hernández Martínez <juan.a.hernandez@grupo-giga.com>
2023-06-18 01:25:01 -0600

    Update SingleComponent route from 'posts' to 'post' and target SingleCategoryComponent specification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import {Route, RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/pages/home/home.component";
-import {CategoryNavbarComponent} from "./components/layouts/category-navbar/category-navbar.component";
 import {SinglePostComponent} from "./components/pages/single-post/single-post.component";
 import {AboutUsComponent} from "./components/pages/about-us/about-us.component";
 import {TermsAndConditionsComponent} from "./components/pages/terms-and-conditions/terms-and-conditions.component";
 import {ContactUsComponent} from "./components/pages/contact-us/contact-us.component";
+import {SingleCategoryComponent} from "./components/pages/single-category/single-category.component";
 
 // Build the routes to access the different pages of the website
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'category', component: CategoryNavbarComponent},
-  { path: 'posts', component: SinglePostComponent},
+  { path: 'category', component: SingleCategoryComponent},
+  { path: 'post', component: SinglePostComponent},
 
   { path: 'about', component: AboutUsComponent},
   { path: 'term-condition', component: TermsAndConditionsComponent},
