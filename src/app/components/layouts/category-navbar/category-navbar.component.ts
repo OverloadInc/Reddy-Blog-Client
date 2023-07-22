96ca7f1d00f8a4d5d4e769359784a8a8c5f3c611
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-22 14:26:00 -0600

    Update CategoryNavbar component to load the available categories

--- src/app/components/layouts/category-navbar/category-navbar.component.ts
+++ src/app/components/layouts/category-navbar/category-navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoriesService} from "../../../services/categories.service";
 
 @Component({
   selector: 'app-category-navbar',
   templateUrl: './category-navbar.component.html',
   styleUrls: ['./category-navbar.component.css']
 })
-export class CategoryNavbarComponent {
+export class CategoryNavbarComponent implements OnInit{
+
+  categoryArray: Array<any> = [];
+
+  constructor(private categoryService: CategoriesService) {
+  }
+
+  ngOnInit(): void {
+    this.categoryService.loadData().subscribe(value => {
+      this.categoryArray = value;
+    });
+  }
 
 }
