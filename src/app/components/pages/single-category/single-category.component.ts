ca8fbc121405412e1a7dde30867c1ab92df01d84
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-22 20:05:24 -0600

    Update component's business logic to get categories.

--- src/app/components/pages/single-category/single-category.component.ts
+++ src/app/components/pages/single-category/single-category.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {PostsService} from "../../../services/posts.service";
 
 @Component({
   selector: 'app-single-category',
   templateUrl: './single-category.component.html',
   styleUrls: ['./single-category.component.css']
 })
-export class SingleCategoryComponent {
+export class SingleCategoryComponent implements OnInit {
+
+  postsArray: Array<object>;
+  categoryObject: any;
+
+  constructor(private route: ActivatedRoute, private postService: PostsService) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(value => {
+
+      this.categoryObject = value;
+
+      this.postService.loadCategoryPosts(value.id).subscribe(post => {
+        this.postsArray = post;
+      });
+    });
+  }
 
 }
