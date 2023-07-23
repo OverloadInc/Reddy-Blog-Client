213c65549dfbbbf4c850bc10d099c625e90e4ac6
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-22 20:00:12 -0600

    Add methods to loadLatest, loadCategoryPosts, loadOnePost, loadSimilar, and countViews

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from "@angular/fire/compat/firestore";
 import {map} from "rxjs";
+import firebase from "firebase/compat/app";
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,58 @@ export class PostsService {
         })
       })
     );
+  }
+
+  loadLatest() {
+    return this.firestore.collection('posts', ref => ref.orderBy('createdAt')).snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+
+          return {id, data}
+        })
+      })
+    );
+  }
+
+  loadCategoryPosts(categoryId) {
+    return this.firestore.collection('posts', ref => ref.where('category.categoryId', '==', categoryId).limit(4)).snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+
+          return {id, data}
+        })
+      })
+    );
+  }
+
+  loadOnePost(postId) {
+    return this.firestore.doc(`posts/${postId}`).valueChanges();
+  }
+
+  loadSimilar(catId) {
+    return this.firestore.collection('posts', ref => ref.where('category.categoryId', '==', catId).limit(4)).snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+
+          return {id, data}
+        })
+      })
+    );
+  }
+
+  countViews(postId) {
+    const viewsCount = {
+      views: firebase.firestore.FieldValue.increment(1)
+    }
+
+    this.firestore.doc(`posts/${postId}`).update(viewsCount).then(() => {
+      console.log('Views count updated');
+    })
   }
 }
