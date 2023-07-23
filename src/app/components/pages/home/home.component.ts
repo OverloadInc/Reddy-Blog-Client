5fe487e36777638b0ff2e2b58812c28feab1ed2b
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-22 19:55:42 -0600

    Add constructor business logic to load featured and latest posts dynamically

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostsService} from "../../../services/posts.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  featuredPostArray: Array<object> = [];
+  latestPostArray: Array<object> = [];
+
+  constructor(private postService: PostsService) {
+    this.postService.loadFeatured().subscribe(value => {
+      this.featuredPostArray = value;
+    });
+
+    this.postService.loadLatest().subscribe(value => {
+      this.latestPostArray = value;
+    });
+  }
+
+  ngOnInit(): void {
+  }
 }
