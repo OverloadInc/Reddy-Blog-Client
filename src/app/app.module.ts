55aa22816c6509e0b0ed0c477b59e440262252c7
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-23 01:12:27 -0600

    Add AngularFirestore, ReactiveForms, and Toastr modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ToastrModule } from "ngx-toastr";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,15 +12,15 @@ import { SingleCategoryComponent } from './components/pages/single-category/sing
 import { TermsAndConditionsComponent } from './components/pages/terms-and-conditions/terms-and-conditions.component';
 import { ContactUsComponent } from './components/pages/contact-us/contact-us.component';
 import { SubscriptionFormComponent } from './components/subscription-form/subscription-form.component';
-import {HomeComponent} from "./components/pages/home/home.component";
-import {SinglePostComponent} from "./components/pages/single-post/single-post.component";
+import { HomeComponent } from "./components/pages/home/home.component";
+import { SinglePostComponent } from "./components/pages/single-post/single-post.component";
 import { CommentFormComponent } from './components/comments/comment-form/comment-form.component';
 import { CommentListComponent } from './components/comments/comment-list/comment-list.component';
 import { AboutUsComponent } from './components/pages/about-us/about-us.component';
 import { PostCardComponent } from './components/layouts/post-card/post-card.component';
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
-import {environment} from "../environments/environment.prod";
+import { environment } from "../environments/environment.prod";
 
 @NgModule({
   declarations: [
@@ -41,7 +43,10 @@ import {environment} from "../environments/environment.prod";
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
