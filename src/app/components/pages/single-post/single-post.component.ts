4000242f0fb9d5b3a7d7072c7e7b05c631d08941
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-22 20:01:56 -0600

    Add methods to load a single post, and similar posts.

--- src/app/components/pages/single-post/single-post.component.ts
+++ src/app/components/pages/single-post/single-post.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {PostsService} from "../../../services/posts.service";
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
   styleUrls: ['./single-post.component.css']
 })
-export class SinglePostComponent {
+export class SinglePostComponent implements OnInit {
 
+  postData: any;
+  similarPostArray: Array<object>;
+
+  constructor(private route: ActivatedRoute, private postService: PostsService) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(value => {
+
+      this.postService.countViews(value.id);
+
+      this.postService.loadOnePost(value.id).subscribe(post => {
+        this.postData = post;
+
+        this.loadSimilarPosts(this.postData.category.categoryId);
+      })
+    })
+  }
+
+  loadSimilarPosts(categoryId) {
+    this.postService.loadSimilar(categoryId).subscribe(value => {
+      this.similarPostArray = value;
+    });
+  }
 }
